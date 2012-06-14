87804cb367dec5b91e1535d7520c97b0eae1e9d5
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-06-14 11:40:54 +0800

    UX Improvements
    * Disables the buttons and other elements upon clicking "add": Show's
    user the selected app and prevents from multiple clicks
    * Spinner gif to show the activity

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
 function appAddRequest() {
 	jQuery('.app_link_button').click(function() {
+		// Indicates the user the item selected and prevents user from clicking multiple times
+		jQuery('.app_link_button').attr("disabled", "disabled");
+		jQuery('.app_view').addClass("disabled");
+		jQuery(this).parent().parent().each(function(index){
+			jQuery(this).removeClass("disabled");
+			jQuery(this).find("h3:first").addClass('ajax_spinner_title');
+			
+		});
+				
+		//Ajax stuff
 		app_id = jQuery(this).attr("id");
 		var data = {
 			action: 'new_app_post',
 			app_id: app_id
 		};
-		
 		jQuery.post(ajaxurl, data, function(response) {
+			}).success(function(response) {
 				window.location.replace(response);
-		});
+			})
+			.error(function() { 
+				alert('Error.. Please try again!');
+				jQuery('.app_link_button').removeAttr('disabled');
+			});
+			
 		return false;
 	});
 };
+
 
 function searchAppstore() {
 	jQuery("#appstore_search_form").submit(function() {
@@ -27,8 +43,13 @@ function searchAppstore() {
 		};
 
 		jQuery.post(ajaxurl, data, function(response) {
-				jQuery('#apps_list').html(response);
-		});
+			
+		}).success(function(response) {
+					jQuery('#apps_list').html(response);
+				})
+			.error(function() { 
+					alert('Error.. Please try again!');
+				});
 		return false;
 	});
 };
